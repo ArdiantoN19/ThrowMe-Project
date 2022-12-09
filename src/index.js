85bb0ad47ce63bf8792a326df1346a889d6da500
ArdiantoN19 <ardi19nugroho@gmail.com>
2022-12-09 19:19:40 +0700

    traspilling mapbox babel

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import bootstrap from "bootstrap";
+// Mapbox
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+
 // Importing Sass with Bootstrap CSS
 import "./App.scss";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+
+// @ts-ignore
+mapboxgl.workerClass =
+  // eslint-disable-next-line import/no-webpack-loader-syntax
+  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
